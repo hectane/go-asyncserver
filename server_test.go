4b116dfab816b09643010647190f1a766702dc91
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-08 16:48:55 -0800

    Added basic test.

---
+++ server_test.go
@@ -0,0 +1,21 @@
+package async
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	s := New(":0")
+	if err := s.Start(); err != nil {
+		t.Fatal(err)
+	}
+	url := "http://" + s.Addr
+	if _, err := http.Get(url); err != nil {
+		t.Fatal(err)
+	}
+	s.Stop()
+	if _, err := http.Get(url); err == nil {
+		t.Fatal("error expected")
+	}
+}
